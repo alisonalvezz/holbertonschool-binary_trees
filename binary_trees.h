57dc75ab234c904ceab306d7651b14f08488dfe1
Alison <8661@holbertonstudents.com>
2024-04-02 15:25:13 +0000

    commit

--- binary_trees.h
+++ binary_trees.h
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct binary_tree_s binary_tree_t;
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -20,8 +22,8 @@ struct binary_tree_s
     struct binary_tree_s *right;
 };
 
-typedef struct binary_tree_s binary_tree_t;
-
+void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 
 #endif
