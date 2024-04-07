f9dd2a6fd41d540c3bf2e25b7536b2cc45175378
root <manuelantognazza@gmail.com>
2024-04-07 09:47:21 -0300

    checks if a binary tree is full

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+*binary_tdasree_is_full - checks if a binary tree is full
+*@tree: pointer to the root node
+*
+*Return: retunr 1 this a tree perfect else 0;
+*/
+
+int binary_tdasree_is_full(const binary_tree_t *tree)
+{
+
+	/* if para saber si es null */
+	if (tree == NULL)
+		return (0);
+
+	/* if para saber si el nodo es completo &&*/
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	/* if ambos sean != NULL &&*/
+	if (tree->right != NULL && tree->left != NULL)
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+
+	return (0);
+}
